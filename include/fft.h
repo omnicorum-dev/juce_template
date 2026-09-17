820b15e074ceeaff264131b20bdb7022cbe9799b
Nico Russo <nicojrusso@gmail.com>
2026-09-17 18:04:00 -0400

    vendored pffft as fft.h backend

---
+++ include/fft.h
@@ -0,0 +1,74 @@
+#pragma once
+
+#include <cassert>
+#include <pffft/pffft.h>
+#include <pffft/pffft_double.h>
+
+#include <complex>
+#include <vector>
+
+namespace omni {
+
+class FFT {
+  public:
+    FFT(size_t size) : _size(size), work(size), buffer(size) {
+        assert(size != 0 && "FFT size cannot be zero");
+        setup = pffftd_new_setup((int)size, PFFFT_REAL);
+        assert(setup_double && "Failed to create PFFFT setup");
+    }
+
+    ~FFT() {
+        if (setup) {
+            pffftd_destroy_setup(setup);
+        }
+    }
+
+    void forward(const double *input, std::complex<double> *output) {
+        pffftd_transform_ordered(
+            setup, input, buffer.data(), work.data(), PFFFT_FORWARD);
+
+        const size_t half = _size / 2;
+
+        output[0] = {buffer[0], 0};
+
+        for (size_t k = 1; k < half; ++k) {
+            output[k] = {buffer[2 * k], buffer[2 * k + 1]};
+        }
+
+        output[half] = {buffer[1], 0};
+    }
+
+    void inverse(const std::complex<double> *input, double *output) {
+        const size_t half = _size / 2;
+
+        buffer[0] = input[0].real();
+        buffer[1] = input[half].real();
+
+        for (size_t k = 1; k < half; ++k) {
+            buffer[2 * k]     = input[k].real();
+            buffer[2 * k + 1] = input[k].imag();
+        }
+
+        pffftd_transform_ordered(
+            setup, buffer.data(), output, work.data(), PFFFT_BACKWARD);
+
+        const double scale = 1 / (double)_size;
+
+        for (size_t i = 0; i < _size; ++i) {
+            output[i] *= scale;
+        }
+    }
+
+    size_t size() const { return _size; }
+    size_t spectrumSize() const { return _size / 2 + 1; }
+
+  private:
+    std::size_t _size;
+
+    PFFFTD_Setup *setup = nullptr;
+
+    std::vector<double> work;
+    std::vector<double> buffer;
+};
+
+} // namespace omni
